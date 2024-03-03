fe8510675339cddaaff9d3211f2b038d91b567ea
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-03-03 21:53:33 +0530

    Designed Nav

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar/>
+        
         <Routes>
            <Route path="/" element={<Home/>}/>
            <Route path="/services" element={<Services/>}/>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,29 +3,32 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-        <header>
-            <div>
+        <header className="shadow"> 
+            <div className={`flex flex-row justify-between pt-4 pb-4 items-center`}>
                 <div>
-                    <NavLink to="/"></NavLink>   {/* this is the left side */}
+                    <NavLink to="/">Amelia</NavLink>   {/* this is the left side AMELIA LOGO */}
                 </div>
                 <nav>
-                    <ul>
+                    <ul className='flex justify-end  items-center'>      {/* this is the right  side AMELIA LOGO */}
 
-                        <li>
+                        <li className=' pr-10'>
                              <NavLink to="/">Home </NavLink>
                          </li>
-                        <li>
+                        <li className=' pr-10'>
                              <NavLink to="/services">Service </NavLink> 
                         </li>
-                        <li>
+                        <li className=' pr-10'>
                              <NavLink to="/gallery">Gallery </NavLink> 
                         </li>
-                        <li>
+                        <li className=' pr-10'>
                              <NavLink to="/contact">Contact us </NavLink> 
                         </li>
-                        
-                    </ul>
+                        <button className='bg-black text-white pr-3 pl-3 pt-2 pb-2 mr-10 ml-10 '>
+                            Book Now
+                            </button>   {/* this is the end of right side */}
+                    </ul>                 
                 </nav>
+
             </div>
         </header>
     </>
