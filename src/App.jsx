645bee3c7ba8d923154814517f2de85a3447e6aa
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-03-05 17:01:59 +0530

    Branch changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from './pages/Services';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import Navbar from './component/Navbar';
+import Booknow from './pages/Booknow';
 
 function App() {
  
@@ -19,6 +20,7 @@ function App() {
              <Route path="/services" element={<Services/>}/>
              <Route path="/gallery" element={<Gallery/>}/>
              <Route path="/contact" element={<Contact/>}/>
+             <Route path="/booknow" element={<Booknow/>}/>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/pages/Booknow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Booknow = () => {
+  return (
+    <div>
+      Bookk now
+    </div>
+  )
+}
+
+export default Booknow
